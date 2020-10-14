ce0444a2274de82ac072dc6923b6a38a5b93f974
Jansan <jansan-26@hotmail.com>
2020-10-14 11:39:47 +0200

    VehicleViewModel created

---
+++ Garage2.0MVC/Models/ViewModels/VehicleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2._0MVC.Models.ViewModels
+{
+    public class VehicleViewModel
+    {
+        public IEnumerable<VehicleModel> VehicleModels { get; set; }
+        public IEnumerable<SelectListItem> VehicleTypes { get; set; }
+        public VehicleType? VehicleType { get; set; }
+        public string RegNum { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+    }
+}
