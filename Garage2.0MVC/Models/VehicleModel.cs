69e6ff5390e8d8c28b7bbc952600289fcee10cd4
joakimmalmstrom <joakim.s.malmstrom@gmail.com>
2020-10-14 09:51:32 +0200

    Model and Type Enum

---
+++ Garage2.0MVC/Models/VehicleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2._0MVC.Models
+{
+    public class VehicleModel
+    {
+        public int Id { get; set; }
+        public Type Type { get; set; }
+        public string RegNum { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumWheels { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+}
