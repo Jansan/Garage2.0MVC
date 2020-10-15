055685b908b16f9ae5530260589c52c277cbdffc
joakimmalmstrom <joakim.s.malmstrom@gmail.com>
2020-10-15 11:01:18 +0200

    Departure Time works

---
+++ Garage2.0MVC/ViewComponents/ReceiptViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Garage2._0MVC.Data;
+using Garage2._0MVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2._0MVC.ViewComponents
+{
+    public class ReceiptViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var departure = DateTime.Now;
+
+            var model = new ReceiptViewModel
+            {
+                DepartureTime = departure
+            };
+
+            return View(model);
+        }
+    }
+}
