a41508a3633dd8c28577f629bf1e100bdeb36021
Murtaza Rafi <murtaza_bayat@hotmail.com>
2020-10-27 13:35:21 +0100

    VehicleModel2 Controller added

---
+++ Garage2.0MVC/Controllers/VehicleModels2Controller.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Garage2._0MVC.Data;
+using Garage2._0MVC.Models;
+
+namespace Garage2._0MVC.Controllers
+{
+    public class VehicleModels2Controller : Controller
+    {
+        private readonly Garage2_0MVCContext _context;
+
+        public VehicleModels2Controller(Garage2_0MVCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VehicleModels2
+        public async Task<IActionResult> Index()
+        {
+            var garage2_0MVCContext = _context.VehicleModel.Include(v => v.Member).Include(v => v.VehicleType);
+            return View(await garage2_0MVCContext.ToListAsync());
+        }
+
+        // GET: VehicleModels2/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleModel = await _context.VehicleModel
+                .Include(v => v.Member)
+                .Include(v => v.VehicleType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels2/Create
+        public IActionResult Create()
+        {
+            ViewData["MemberId"] = new SelectList(_context.Member, "Id", "Id");
+            ViewData["VehicleTypeId"] = new SelectList(_context.VehicleType, "Id", "Id");
+            return View();
+        }
+
+        // POST: VehicleModels2/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Type,RegNum,Color,Brand,Model,NumWheels,ArrivalTime,VehicleTypeId,MemberId")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicleModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Member, "Id", "Id", vehicleModel.MemberId);
+            ViewData["VehicleTypeId"] = new SelectList(_context.VehicleType, "Id", "Id", vehicleModel.VehicleTypeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels2/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleModel = await _context.VehicleModel.FindAsync(id);
+            if (vehicleModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["MemberId"] = new SelectList(_context.Member, "Id", "Id", vehicleModel.MemberId);
+            ViewData["VehicleTypeId"] = new SelectList(_context.VehicleType, "Id", "Id", vehicleModel.VehicleTypeId);
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels2/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,RegNum,Color,Brand,Model,NumWheels,ArrivalTime,VehicleTypeId,MemberId")] VehicleModel vehicleModel)
+        {
+            if (id != vehicleModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicleModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleModelExists(vehicleModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Member, "Id", "Id", vehicleModel.MemberId);
+            ViewData["VehicleTypeId"] = new SelectList(_context.VehicleType, "Id", "Id", vehicleModel.VehicleTypeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels2/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleModel = await _context.VehicleModel
+                .Include(v => v.Member)
+                .Include(v => v.VehicleType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels2/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vehicleModel = await _context.VehicleModel.FindAsync(id);
+            _context.VehicleModel.Remove(vehicleModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleModelExists(int id)
+        {
+            return _context.VehicleModel.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Garage2.0MVC/Migrations/20201027122749_Init.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage2._0MVC.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParkingSpace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParkingNum = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkingSpace", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehicleType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(nullable: false),
+                    Capacity = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehicleModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(nullable: false),
+                    RegNum = table.Column<string>(maxLength: 6, nullable: false),
+                    Color = table.Column<string>(maxLength: 20, nullable: false),
+                    Brand = table.Column<string>(maxLength: 25, nullable: false),
+                    Model = table.Column<string>(maxLength: 25, nullable: false),
+                    NumWheels = table.Column<int>(nullable: false),
+                    ArrivalTime = table.Column<DateTime>(nullable: false),
+                    VehicleTypeId = table.Column<int>(nullable: false),
+                    MemberId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VehicleModel_Member_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Member",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VehicleModel_VehicleType_VehicleTypeId",
+                        column: x => x.VehicleTypeId,
+                        principalTable: "VehicleType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehicleModelParkingSpace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleModelId = table.Column<int>(nullable: false),
+                    ParkingSpaceId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleModelParkingSpace", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VehicleModelParkingSpace_ParkingSpace_ParkingSpaceId",
+                        column: x => x.ParkingSpaceId,
+                        principalTable: "ParkingSpace",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VehicleModelParkingSpace_VehicleModel_VehicleModelId",
+                        column: x => x.VehicleModelId,
+                        principalTable: "VehicleModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "VehicleType",
+                columns: new[] { "Id", "Capacity", "Type" },
+                values: new object[,]
+                {
+                    { 1, 1.0, 0 },
+                    { 2, 0.29999999999999999, 1 },
+                    { 3, 2.0, 2 },
+                    { 4, 2.0, 3 },
+                    { 5, 3.0, 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleModel_MemberId",
+                table: "VehicleModel",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleModel_VehicleTypeId",
+                table: "VehicleModel",
+                column: "VehicleTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleModelParkingSpace_ParkingSpaceId",
+                table: "VehicleModelParkingSpace",
+                column: "ParkingSpaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleModelParkingSpace_VehicleModelId",
+                table: "VehicleModelParkingSpace",
+                column: "VehicleModelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehicleModelParkingSpace");
+
+            migrationBuilder.DropTable(
+                name: "ParkingSpace");
+
+            migrationBuilder.DropTable(
+                name: "VehicleModel");
+
+            migrationBuilder.DropTable(
+                name: "Member");
+
+            migrationBuilder.DropTable(
+                name: "VehicleType");
+        }
+    }
+}
