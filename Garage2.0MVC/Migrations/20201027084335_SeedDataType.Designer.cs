214a85b534e2c225a8428aa35186be53cc7bde72
joakimmalmstrom <joakim.s.malmstrom@gmail.com>
2020-10-27 09:45:24 +0100

    Seed data for VehicleType

--- Garage2.0MVC/Migrations/20201027084335_SeedDataType.Designer.cs
+++ Garage2.0MVC/Migrations/20201027084335_SeedDataType.Designer.cs
@@ -4,14 +4,16 @@ using Garage2._0MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Garage2._0MVC.Migrations
 {
     [DbContext(typeof(Garage2_0MVCContext))]
-    partial class Garage2_0MVCContextModelSnapshot : ModelSnapshot
+    [Migration("20201027084335_SeedDataType")]
+    partial class SeedDataType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
