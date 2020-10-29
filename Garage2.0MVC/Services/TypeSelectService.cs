035880125f179f5760cb7fdc53ca4460e3493a9b
Murtaza Rafi <murtaza_bayat@hotmail.com>
2020-10-29 16:15:21 +0100

    Disabled type

--- Garage2.0MVC/Services/TypeSelectService.cs
+++ Garage2.0MVC/Services/TypeSelectService.cs
@@ -13,21 +13,27 @@ namespace Garage2._0MVC.Services
     {
         private readonly Garage2_0MVCContext db;
         private readonly IParkingService parkingService;
+        private readonly IParkingCapacityService parkingCapacityService;
 
-        public TypeSelectService(Garage2_0MVCContext db, IParkingService parkingService)
+        public TypeSelectService(Garage2_0MVCContext db, IParkingService parkingService, IParkingCapacityService parkingCapacityService)
         {
             this.db = db;
             this.parkingService = parkingService;
+            this.parkingCapacityService = parkingCapacityService;
         }
 
+        
         public async Task<IEnumerable<SelectListItem>> GetTypesAsync()
         {
+            var space = (double)parkingService.GetCurrentParking();
+            var capacity = parkingCapacityService.GetVehicleCapacity();
             return await db.VehicleType
                         .Select
                         (v => new SelectListItem
                         {
                             Text = v.Type.ToString(),
-                            Value = v.Type.ToString()
+                            Value = v.Type.ToString(),
+                            Disabled = space<v.Capacity? true : false
                         })
                         .ToListAsync();
         }
